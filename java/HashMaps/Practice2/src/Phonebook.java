bed84798f6f02507cc2ee7e069a8cb389a3761e7
Eric Zhang <ericbkzhang@gmail.com>
2023-12-02 16:35:32 -0500

    Theoretically finished Phonebook.java but haven't test it yet

--- java/HashMaps/Practice2/src/Phonebook.java
+++ java/HashMaps/Practice2/src/Phonebook.java
@@ -1,7 +1,31 @@
 import java.util.HashMap;
 
 public class Phonebook {
-    public static void 
+    /**
+     * requests user to input three Strings as a phone number
+     * @return a PhoneNumber object based on user input
+     */
+    public static PhoneNumber inputPhoneNumber() {
+        Scanner input = new Scanner(System.in);
+
+        String phoneNum = "";
+
+        // until the user enters a String of exactly seven digits
+        do {
+            // ask them to input a phone number
+            System.out.println("Please enter a phone number without any dashes(-) or other formatting(i.e. XXXXXXXXXX): ");
+            phoneNum = input.nextLine();
+
+        } while (!phoneNum.matches("\d{7}"));
+        
+        // split the phone number into three
+        // and pass into a new PhoneNumber to return
+        return new PhoneNumber(
+            phoneNum.substring(0, 3), 
+            phoneNum.substring(3, 6), 
+            phoneNum.substring(6, 10)
+        );
+    }
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -16,8 +40,15 @@ public class Phonebook {
             name = input.nextLine();
 
             // if the name is not in the phonebook
-            if (!phonebook.keySet().contains(name)) {
-                
+            // and the name is not blank
+            if (!phonebook.keySet().contains(name) && !name.equals("")) {
+                // ask for the phone number
+                // and store it into phonebook with name
+                phonebook.put(name, inputPhoneNumber());
+
+            } else if (phonebook.keySet.contains(name)) { // if the name is in phonebook
+                // print out the name
+                System.out.println(phonebook.get(name));
             }
 
         } while (userInput.equals(""));
